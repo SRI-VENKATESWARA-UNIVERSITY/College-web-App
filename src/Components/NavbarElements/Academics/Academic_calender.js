ad0d45ea5d692a10defb49d96e730798145495d6
svucewebdev <svuce.webdevelopers@gmail.com>
2025-05-02 10:50:55 +0530

    updated academic calender

--- src/Components/NavbarElements/Academics/Academic_calender.js
+++ src/Components/NavbarElements/Academics/Academic_calender.js
@@ -1,13 +1,44 @@
-import { Container,Row,Col, Card, CardBody, CardTitle, CardSubtitle, Table} from "reactstrap"
+import { Container, Row, Col, Card, CardBody, CardTitle } from "reactstrap";
 
-const calender = () =>{
-     return(
-<Container className="timetable">
-    <Row>   
-                        <p>Click here to Download Academic Calendar for 2023-2024- <a href="/academics/academic schedule.pdf">click</a></p>
-    </Row>
+const Calendar = () => {
+  const calendarData = [
+    {
+      year: "2024-2025 Btech",
+      file: "https://drive.google.com/file/d/1OU7FOXdWTSs0mMs7Ntgk3zw8jamu2bk5/view?usp=sharing",
+    },
+    {
+      year: "2023-2024 Btech",
+      file: "https://drive.google.com/file/d/1eeSWTqbtXq-UTfkFXcsJkaKAW7jjDWNw/view?usp=sharing",
+    },
+    {
+      year: "2022-2023 Btech",
+      file: "https://drive.google.com/file/d/1K7n9gCNifwR_HjVKOh-Ku9W3KVqO17td/view?usp=sharing",
+    },
+    {
+      year: "2022,2023,2024 Mtech",
+      file: "https://drive.google.com/file/d/1LAN6yCx-AfsXBLvUzfvp5JIf_ftDRA_5/view?usp=sharing",
+    },
+  ];
 
-</Container>
-     ) 
-}
-export default calender
+  return (
+    <Container className="timetable my-4">
+      <Row>
+        {calendarData.map((item, index) => (
+          <Col sm="6" md="4" lg="3" key={index} className="mb-4">
+            <Card>
+              <CardBody>
+                <CardTitle tag="h5">Academic Calendar</CardTitle>
+                <p>{item.year}</p>
+                <a href={item.file} download>
+                  Download PDF
+                </a>
+              </CardBody>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Calendar;
