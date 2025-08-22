3f4e501fb71a26a11879f0bf9d5b96dc9b4f87be
svucewebdev <svuce.webdevelopers@gmail.com>
2025-08-22 15:31:35 +0530

    new update

--- src/Components/NavbarElements/NEWS.js
+++ src/Components/NavbarElements/NEWS.js
@@ -1,104 +1,52 @@
+import { Col, Container, Row } from "reactstrap";
 
-import { Col, Container ,ListGroup,ListGroupItem,Row} from "reactstrap"
+export const newsData = [
+  {
+    "name": "I B. Tech Orientation Programme Schedule on 25-08-2025",
+    "Filename": "/news/2025orientation.PDF"
+  },
+  {
+    "name": "I B.Tech Orientation Programme on 20-01-2021",
+    "Filename": "/news/IbtechOrientation.PDF"
+  },
 
+];
 
-import {useState,useEffect} from "react";
-import axios from "axios"
-import { FaEdit } from "react-icons/fa";
-import { BsFillXSquareFill } from "react-icons/bs";
-import AddNewsModal from "../MainPageComp/AddNewsModal";
-import DeleteNewsModal from "../MainPageComp/DeleteNewsModal";
-const NEWS = () =>{
-  const [user,SetUser] = useState({admin:false})
-  useEffect(()=>{
-    var ls = (JSON.parse(localStorage.getItem("user")))
-     if(ls)
-     {
-     if(ls._id){
-       axios.get(`https://svuceedu.netlify.app/.netlify/functions/server/auth/${ls._id}`)
-       .then((resp)=>{
-      SetUser(resp.data)
-       })
-       .catch((err)=>{SetUser({admin:false})})
-     }
-    }
-    
-    }
-  ,[])
-  const [newsEditClicked,SetNewsEditClicked] = useState(false)
-  const handleNewsEdit = ()=>{
-       SetNewsEditClicked(!newsEditClicked);
-  }
-  const [news,SetNews] = useState([]);
-  useEffect(()=>{
-    axios.get("https://svuceedu.netlify.app/.netlify/functions/server/news")
-    .then((resp)=>{
-  
-  SetNews(resp.data.news.reverse())
-    })
-  },[])
-   return(
-       <Container>
-           <Row>
-              
-               <Col>
-           <div style={{marginTop:100,backgroundColor:"#d0efff",borderTop:"6px solid blue" ,padding:"10px 20px 10px 20px",marginBottom:100}}> 
-             
-             <h2 style={{color:"blue"}}>NEWS
-             {/* FA EDIT ICON */}
-             {user.admin?(
-             <>
-                 {!newsEditClicked ? (
-                  <>
-                    <FaEdit onClick={handleNewsEdit} className="Fanews" style={{right:30,fontSize:30,position:"absolute",paddingTop:9}}/>
+const NEWS = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <div
+            style={{
+              marginTop: 100,
+              backgroundColor: "#d0efff",
+              borderTop: "6px solid blue",
+              padding: "10px 20px",
+              marginBottom: 100
+            }}
+          >
+            <h2 style={{ color: "blue" }}>NEWS</h2>
 
-                 </>):
-             (
-                 <>
-                 <BsFillXSquareFill  onClick={handleNewsEdit} style={{right:20,fontSize:30,position:"absolute",paddingTop:10}} className="Fanews" color="red"/>
-                 </>)}
-             </>)
-             :
-             (
-             <>
-             </>
-             )}
-             </h2>
-             {newsEditClicked?(
-             <>
-              <div>
-                 <ListGroup>
-                 <AddNewsModal news={news} SetNews={SetNews}/>
-               
-                 {news.map((word,index)=>(
-                  <ListGroupItem href={word.DriveLink} style={{color:"darkviolet"}}>
-                     <a href={word.DriveLink} style={{color:"rgb(107 178 255)"}}><p><span style={{fontSize:20}}>{index+1}</span>. {word.newsHeading}</p></a>
-                    <DeleteNewsModal news_id = {word._id}  SetNews = {SetNews} news_heading = {word.newsHeading}/>
-                  </ListGroupItem>
-                 ))}
-                 </ListGroup>
-                </div>
-               
-             </>)
-             :
-             (
-             <>
-             <div className="news">
-                 {news.map((word,index)=>(
-                  <a href={word.DriveLink} style={{color:"rgb(107 178 255)"}}><p><span style={{fontSize:20}}>{index+1}</span>. {word.newsHeading}</p></a>
-                 ))}
-               </div>
-             </>
-             )}
-               
-              
-               
-           </div>
-               </Col>
-               
-           </Row>
-       </Container>
-   )
-}
+            <div className="news">
+              {newsData.map((item, index) => (
+                <a
+                  key={index}
+                  href={item.Filename}
+                  style={{ color: "rgb(107 178 255)" }}
+                >
+                  <p>
+                    <span style={{ fontSize: 20 }}>{index + 1}</span>.{" "}
+                    {item.name}
+                  </p>
+                </a>
+              ))}
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default NEWS
+export default NEWS;
--- src/assets/DBStatic/news.js
+++ src/assets/DBStatic/news.js
@@ -1,5 +1,9 @@
 export const news = [
     {
+        "name" : "I B. Tech Orientation Programme Schedule on 25-08-2025",
+        "Filename" : "/news/2025orientation.PDF"
+    },
+    {
         "name" : "I B.Tech Orientation Programme on 20-01-2021",
         "Filename" : "/news/IbtechOrientation.PDF"
     },
